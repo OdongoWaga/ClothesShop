fe3ff5d76b2781bbfc82a8e4ecc12cf57d3eae85
Waga <wagaodongo@gmail.com>
2019-09-02 21:22:51 +0300

    productDetailsPage

--- lib/pages/productDetails.dart
+++ lib/pages/productDetails.dart
@@ -46,10 +46,125 @@ class _ProductDetailsState extends State<ProductDetails> {
           Container(
             height: 300.0,
             child: GridTile(
-                child: Container(
-              color: Colors.white70,
-              child: Image.asset(widget.productDetailPicture),
-            )),
+              child: Container(
+                color: Colors.white70,
+                child: Image.asset(widget.productDetailPicture),
+              ),
+              footer: Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(
+                    "${widget.productDetailName}",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          "\$ ${widget.productDetailOldPrice}",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "\$${widget.productDetailPrice}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.red),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text("Size"),
+                      ),
+                      Expanded(
+                        child: Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text("Color"),
+                      ),
+                      Expanded(
+                        child: Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text("Qty"),
+                      ),
+                      Expanded(
+                        child: Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  elevation: 0.2,
+                  child: Text('Buy'),
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.add_shopping_cart,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.favorite_border,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
+              ),
+            ],
           )
         ],
       ),
